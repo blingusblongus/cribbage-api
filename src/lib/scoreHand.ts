72296f06bff72f619acba14d3ab43c4505dee656
Nick Allen <rutherford.t.b@gmail.com>
2024-05-18 22:02:18 -0500

    refactor scoreHand logic and support up to 6 cards

---
+++ src/lib/scoreHand.ts
@@ -0,0 +1,56 @@
+import { Card } from "./Card"
+import { Deck } from "./Deck"
+import { Hand } from "./Hand";
+
+export const scoreHand = (cards: Card[]) => {
+    // Init Deck
+    const deck = new Deck();
+
+    // Remove Provided cards from deck
+    cards.forEach(({ rank, suit }) => deck.removeCard(rank, suit))
+
+    // Check each combo (15x46 iterations)
+    const scoredHands:
+        { [key: number]: { count: number; chance: number; flips: { rank: string; suit: string; count: number; }[] } } = {};
+
+    // Add each potential flip card
+    for (let i = 0; i < deck.cards.length; ++i) {
+        const hand = new Hand(cards, deck.cards[i]);
+
+        const score = hand.printCounts().total;
+        if (scoredHands[score]) {
+            scoredHands[score].count++,
+                scoredHands[score].flips.push(deck.cards[i].print())
+            scoredHands[score].chance = (scoredHands[score].count / 48) * 100
+        } else {
+            scoredHands[score] = {
+                count: 1,
+                flips: [deck.cards[i].print()],
+                chance: 1 / 48,
+            };
+        }
+    }
+    const scores = Object.keys(scoredHands)
+        .map(key => Number(key))
+        .sort((a, b) => a - b);
+    const max = Math.max(...scores);
+    const min = Math.min(...scores);
+    const total = scores.reduce((sum, el) => sum += el, 0);
+    const count = scores.length;
+    const mean = total / count;
+
+    const middle = Math.floor(scores.length / 2);
+    const median = scores.length % 2 === 0 ? (scores[middle - 1] + scores[middle]) / 2 : scores[middle];
+
+    const variance = scores.reduce((acc, score) => acc + (score - mean) ** 2, 0) / scores.length;
+    const standardDeviation = Math.sqrt(variance);
+
+    return {
+        max,
+        min,
+        mean,
+        median,
+        standardDeviation,
+        scoringOptions: scoredHands,
+    }
+}
--- src/pages/api/handStats.ts
+++ src/pages/api/handStats.ts
@@ -4,6 +4,7 @@ import { parseCardsQuery } from "../../utils/parseCardsQuery";
 import type { Card } from "../../lib/Card";
 import { getCombinations } from "../../utils/getCombinations";
 import { Hand } from "../../lib/Hand";
+import { scoreHand } from "../../lib/scoreHand";
 
 export const GET: APIRoute = (context) => {
     console.time("handsStats");
@@ -25,62 +26,24 @@ export const GET: APIRoute = (context) => {
     if (parsedCards.length > 6) {
         return new Response("Hand size is limited to 6", { status: 400 })
     }
-    // Init Deck
-    const deck = new Deck();
 
-    // Remove Provided cards from deck
-    parsedCards.forEach(({ rank, suit }) => deck.removeCard(rank, suit))
-    deck.shuffle();
+    if (parsedCards.length === 4) {
+        return new Response(JSON.stringify(scoreHand(parsedCards), null, 2))
+    }
+
+    const collectedResults = [];
 
     // Get 4-card combinations
     const combos = getCombinations(parsedCards, 4);
-
-    // Check each combo (15x46 iterations)
-    const scoredHands:
-        { [key: number]: { count: number; chance: number; flips: { rank: string; suit: string; count: number; }[] } } = {};
     for (let combo of combos) {
-        // Add each potential flip card
-        for (let i = 0; i < deck.cards.length; ++i) {
-            const hand = new Hand(combo, deck.cards[i]);
-
-            const score = hand.printCounts().total;
-            if (scoredHands[score]) {
-                scoredHands[score].count++,
-                    scoredHands[score].flips.push(deck.cards[i].print())
-                scoredHands[score].chance = (scoredHands[score].count / 48) * 100
-            } else {
-                scoredHands[score] = {
-                    count: 1,
-                    flips: [deck.cards[i].print()],
-                    chance: 1 / 48,
-                };
-            }
-        }
+        collectedResults.push({
+            keep: combo.map(c => c.print()),
+            result: scoreHand(combo),
+        });
     }
 
-    const scores = Object.keys(scoredHands)
-        .map(key => Number(key))
-        .sort((a, b) => a - b);
-    const max = Math.max(...scores);
-    const min = Math.min(...scores);
-    const total = scores.reduce((sum, el) => sum += el, 0);
-    const count = scores.length;
-    const mean = total / count;
 
-    const middle = Math.floor(scores.length / 2);
-    const median = scores.length % 2 === 0 ? (scores[middle - 1] + scores[middle]) / 2 : scores[middle];
-
-    const variance = scores.reduce((acc, score) => acc + (score - mean) ** 2, 0) / scores.length;
-    const standardDeviation = Math.sqrt(variance);
-
-    const result = {
-        max,
-        min,
-        mean,
-        median,
-        standardDeviation,
-        scoringOptions: scoredHands,
-    }
+    const result = collectedResults.sort((a, b) => b.result.mean - a.result.mean)
 
     console.timeEnd("handsStats");
     return new Response(JSON.stringify(result, null, 2))
